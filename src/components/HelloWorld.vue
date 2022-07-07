c0a963e168400ac7a027be2a555bb49ae8c1069c
niceplugin <niceplugin@gmail.com>
2022-07-08 01:51:50 +0900

    페이지 텍스트 변경 테스트

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,27 +10,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <p>깃허브 배포 자동화 어렵구만</p>
 </template>
 
 <style scoped>
